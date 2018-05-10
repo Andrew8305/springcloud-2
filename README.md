5e14e062b005e855f673ab8480fb11ae121f36fb
Robert Rong <robert.rong@agile-solutions.ch>
2018-05-10 15:56:39 +0200

    again refactor

--- README.md
+++
@@ -1,8 +0,0 @@
-# Spring Cloud modules demonstrated
-changes applied to original version https://github.com/azarai/spring-boot-intermediate-microservices
-## changes
-1. Change native to GIT config store
-
-## readings
-1. [spring cloud tutorial](http://www.baeldung.com/spring-cloud-tutorial)
-2. [spring cloud io](http://projects.spring.io/spring-cloud/)
--- config/src/main/java/de/codeboje/springbootbook/registry/ConfigServer.java
+++ config/src/main/java/de/codeboje/springbootbook/registry/ConfigServer.java
@@ -1,13 +1,15 @@
 package de.codeboje.springbootbook.registry;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 @EnableConfigServer
 public class ConfigServer {
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServer.class, args);
+		new SpringApplicationBuilder(ConfigServers.class).properties("spring.config.name=configserver")
+				.run(args);
 	}
 }
