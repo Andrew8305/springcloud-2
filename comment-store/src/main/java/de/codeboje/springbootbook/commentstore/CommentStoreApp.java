bf7fcca0f652b6dfaed0b3fb331a1c279eca47aa
Robert Rong <robert.rong@agile-solutions.ch>
2018-05-13 14:35:38 +0200

    fixing maven build problem, finally mvn clean installs

--- comment-store/src/main/java/de/codeboje/springbootbook/commentstore/CommentStoreApp.java
+++ comment-store/src/main/java/de/codeboje/springbootbook/commentstore/CommentStoreApp.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
--- config/src/main/java/de/codeboje/springbootbook/registry/ConfigServer.java
+++ config/src/main/java/de/codeboje/springbootbook/registry/ConfigServer.java
@@ -1,9 +1,12 @@
 package de.codeboje.springbootbook.registry;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
-@EnableConfigServer
+@Configuration
+@EnableAutoConfiguration
 @SpringBootApplication
 public class ConfigServer {
 	public static void main(String[] args) {
